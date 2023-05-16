c4bef42b44a6acd0b5d6cd2b4d493e6414813fee
Matthew Knudsvig <66237096+matthewknudsvig@users.noreply.github.com>
2023-05-16 14:27:13 -0400

    set up openai stream TO DO finish setting up

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -1,4 +1,5 @@
-import { ChatGPTMessage } from "@/lib/openai-stream"
+import { chatbotPrompt } from "@/app/helpers/constants/chatbot-prompt"
+import { ChatGPTMessage, OpenAIStream, OpenAIStreamPayload } from "@/lib/openai-stream"
 import { MessageArraySchema } from "@/lib/validators/message"
 
 export async function POST(req: Request) {
@@ -15,4 +16,20 @@ export async function POST(req: Request) {
         role: 'system',
         content: chatbotPrompt
     })
+
+    const payload: OpenAIStreamPayload = {
+        model: 'gpt-3.5-turbo',
+        messages: outboundMessages,
+        temperature: 0.4,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+        max_tokens: 150,
+        stream: true,
+        n: 1,
+    }
+
+    const stream = await OpenAIStream(payload)
+
+    return new Response(stream)
 }
