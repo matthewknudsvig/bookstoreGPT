f6be4511b936f909eafa6fd2685b500b9b7d8d02
Matthew Knudsvig <66237096+matthewknudsvig@users.noreply.github.com>
2023-05-16 21:04:34 -0400

    fixed rounded corners for messages, started error handling TO DO finish error handling

--- src/app/components/ChatMessages.tsx
+++ src/app/components/ChatMessages.tsx
@@ -2,6 +2,7 @@
 import { MessagesContext } from '@/context/messages'
 import { cn } from '@/lib/utils'
 import { FC, HTMLAttributes, useContext } from 'react'
+import MarkdownLite from './MarkdownLite'
 
 interface ChatMessageProps extends HTMLAttributes<HTMLDivElement> {}
 
@@ -25,12 +26,17 @@ const ChatMessages: FC<ChatMessageProps> = ({ className, ...props}) => {
                         <div className={cn(
                         'flex flex-col space-y-2 text-sm max-w-xs mx-2 overflow-x-hidden',
                         {
-                            'bg-blue-600 text-white': message.isUserMessage,
-                            'bg-gray-200 text-gray-900': !message.isUserMessage,
+                            'order-1 items-end': message.isUserMessage,
+                            'order-2 items-start': !message.isUserMessage,
                         }
                         )}>
-                            {/* <MarkdownLite text={message.text} /> */}
-                            {message.text}
+                            <p 
+                            className={cn('px-4 py-2 rounded-lg', {
+                                'bg-blue-600 text-white': message.isUserMessage,
+                                'bg-gray-200 text-gray-900': !message.isUserMessage,
+                            })}>
+                            <MarkdownLite text={message.text} />
+                            </p>
                         </div>
                 </div>
             </div>
--- src/app/components/Providers.tsx
+++ src/app/components/Providers.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { MessagesProvider } from '@/context/messages';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { FC, ReactNode } from "react";
 
@@ -9,6 +10,6 @@ interface ProviderProps {
 
 const Providers: FC<ProviderProps> = ({children}) => {
     const queryClient = new QueryClient()
-    return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    return <QueryClientProvider client={queryClient}><MessagesProvider>{children}</MessagesProvider></QueryClientProvider>
 }
 export default Providers
